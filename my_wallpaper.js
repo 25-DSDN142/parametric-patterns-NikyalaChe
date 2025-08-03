a369b5f3e946a2d21ced342762941b34c2352265
NikyalaChe <symonsniky@myvuw.ac.nz>
2025-08-03 18:11:14 +1200

    created a function for my mountains
    after some trial and error of finding out the right code to use to move them over - i asked for some help from copilot on what wasn't working in my code. I has created a variable for the coordinates but this didnt load my code. I was able to identify that I didn't need a variable for such a function and could easily make Mountains its own function calling x,y and having the option of changing these to move the image

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -2,8 +2,9 @@
 let mountainBackColour = [143, 133, 135]; //light grey colour
 let mountainFrontColour = [127, 140, 104]; //green colour
 
-let mountainStart = 100;  //position of the front and back mountains
-let mountainFinish = 100; //position of the front and back mountains
+let mountainStart = 100;  //start position of the front and back mountains
+let mountainFinish = 100; //finish position of the front and back mountains
+
 
 
 function setup_wallpaper(pWallpaper) {
@@ -25,43 +26,46 @@ function wallpaper_background() {
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
 
+  mountains(20, 40);  
 
+}
 
+function mountains(x, y) {
   //mountains 
 
   //mountainBack 
   fill(143, 133, 135); //light grey colour
   noStroke();
-  triangle(0, mountainStart, 30, 30, 60, mountainFinish); //triangle 1 back
-  triangle(10, mountainStart, 60, 10, 120, mountainFinish); //triangle 2 back
-  triangle(50, mountainStart, 90, 10, 150, mountainFinish); //triangle 3 back 
-  triangle(70, mountainStart, 130, 20, 180, mountainFinish); //triangle 4 back 
+  triangle(0 + x, mountainStart + y, 30 + x, 30 + y, 60 + x, mountainFinish + y,); //triangle 1 back
+  triangle(10 + x, mountainStart + y, 60 + x, 10 + y, 120 + x, mountainFinish + y,); //triangle 2 back
+  triangle(50 + x, mountainStart + y, 90 + x, 10 + y, 150 + x, mountainFinish + y,); //triangle 3 back 
+  triangle(70 + x, mountainStart + y, 130 + x, 20 + y, 180 + x, mountainFinish + y,); //triangle 4 back 
   
   //mountainbackShadow
   fill(61, 57, 58); //dark grey colour
   noStroke();
-  triangle(0, 100, 30, 30, 10, 100); //triangle 1 back shadow
-  triangle(10, 100, 60, 10, 40, 100); //triangle 2 back shadow 
-  triangle(50, 100, 90, 10, 70, 100); //triangle 3 back shadow
-  triangle(70, 100, 130, 20, 110, 100); //triangle 4 back shadow
+  triangle(0 + x, 100 + y, 30 + x, 30 + y, 10 + x, 100 + y); //triangle 1 back shadow
+  triangle(10 + x, 100 + y, 60 + x, 10 + y, 40 + x, 100 + y); //triangle 2 back shadow 
+  triangle(50 + x, 100 + y, 90 + x, 10 + y, 70 + x, 100 + y); //triangle 3 back shadow
+  triangle(70 + x, 100 + y, 130 + x, 20 + y, 110 + x, 100 + y); //triangle 4 back shadow
   
 //mountainFront
   fill(127, 140, 104); //green colour
   noStroke();
-  triangle(5, mountainStart, 30, 50, 50, mountainFinish); //triangle 1 front
-  triangle(30, mountainStart, 60, 30, 100, mountainFinish); //triangle 2 front
-  triangle(60, mountainStart, 110, 10, 150, mountainFinish); //triangle 3 front
-  triangle(120, mountainStart, 150, 50, 170, mountainFinish); //triangle 4 front
-  triangle(130, mountainStart, 170, 40, 200, mountainFinish); //triangle 5 front
+  triangle(5 + x, mountainStart + y, 30 + x, 50 + y, 50 + x, mountainFinish + y); //triangle 1 front
+  triangle(30 + x, mountainStart + y, 60 + x, 30 + y, 100 + x, mountainFinish + y); //triangle 2 front
+  triangle(60 + x, mountainStart + y, 110 + x, 10 + y, 150 + x, mountainFinish + y); //triangle 3 front
+  triangle(120 + x, mountainStart + y, 150 + x, 50 + y, 170 + x, mountainFinish + y); //triangle 4 front
+  triangle(130 + x, mountainStart + y, 170 + x, 40 + y, 200 + x, mountainFinish + y); //triangle 5 front
 
 //mountainFrontShadow
   fill(85, 92, 72); //dark green colour
   noStroke();
-  triangle(5, 100, 30, 50, 20, 100); //triangle 1 front shadow
-  triangle(30, 100, 60, 30, 40, 100); //triangle 2 front shadow
-  triangle(60, 100, 110, 10, 80, 100); //triangle 3 front shadow
-  triangle(120, 100, 150, 50, 130, 100); //triangle 4 front shadow
-  triangle(130, 100, 170, 40, 150, 100); //triangle 5 front shadow
+  triangle(5 + x, 100 + y, 30 + x, 50 + y, 20 + x, 100 + y); //triangle 1 front shadow
+  triangle(30 + x, 100 + y, 60 + x, 30 + y, 40 + x, 100 + y); //triangle 2 front shadow
+  triangle(60 + x, 100 + y, 110 + x, 10 + y, 80 + x, 100 + y); //triangle 3 front shadow
+  triangle(120 + x, 100 + y, 150 + x, 50 + y, 130 + x, 100 + y); //triangle 4 front shadow
+  triangle(130 + x, 100 + y, 170 + x, 40 + y, 150 + x, 100 + y); //triangle 5 front shadow
 
 
 
